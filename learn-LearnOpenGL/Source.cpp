c00d957c3d542dfccb2d6931900d3d44dbd3cf70
Doner357 <don468sam@gmail.com>
2023-01-28 19:33:03 +0800

    Correct wrong words

--- learn-LearnOpenGL/Source.cpp
+++ learn-LearnOpenGL/Source.cpp
@@ -151,7 +151,7 @@ int main(void) {
 	* --------------------------------------------------------------------------------------------------------------------
 	*/
 
-	float verticies[]{
+	float vertices[]{
 		-0.5f, -0.5f, 0.0f,     // bottom-left
 		 0.5f, -0.5f, 0.0f,     // bottom-right
 		 0.0f,  0.5f, 0.0f      // top
@@ -168,7 +168,7 @@ int main(void) {
 	// Bind the VBO
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	// Copy the vertex data to into the buffer's memory
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	// Set vertex attributes pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
