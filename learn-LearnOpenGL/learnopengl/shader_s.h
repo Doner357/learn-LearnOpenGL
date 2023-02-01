3694cbf5c9bf212d1cd5e178736d60498242845d
Doner357 <don468sam@gmail.com>
2023-02-01 16:23:50 +0800

    Fix the shader checking bug

--- learn-LearnOpenGL/learnopengl/shader_s.h
+++ learn-LearnOpenGL/learnopengl/shader_s.h
@@ -37,10 +37,10 @@ public:
 			vShaderFile.close();
 			fShaderFile.close();
 			// Convert stream into string
-			vertexCode   = vShaderStream.str();
+			vertexCode = vShaderStream.str();
 			fragmentCode = fShaderStream.str();
 		}
-		catch(std::ifstream::failure e){
+		catch (std::ifstream::failure e) {
 			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
 		}
 		const char *vShaderCode = vertexCode.c_str();
@@ -64,7 +64,7 @@ public:
 		glShaderSource(fragment, 1, &fShaderCode, NULL);
 		glCompileShader(fragment);
 		// Print compile errors if any
-		checkCompileErrors(vertex, "FRAGMENT");
+		checkCompileErrors(fragment, "FRAGMENT");
 
 		// shader program
 		ID = glCreateProgram();
@@ -72,7 +72,7 @@ public:
 		glAttachShader(ID, fragment);
 		glLinkProgram(ID);
 		// Print linking errors if any
-		checkCompileErrors(vertex, "PROGRAM");
+		checkCompileErrors(ID, "PROGRAM");
 
 		// Delete the shaders ad they're linked into our program and no longer necessary
 		glDeleteShader(vertex);
