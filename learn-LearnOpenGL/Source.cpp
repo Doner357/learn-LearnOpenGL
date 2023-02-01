faf5992d23d8361e3ce0172f587344bd0427d756
Doner357 <don468sam@gmail.com>
2023-02-01 15:34:11 +0800

    Add the content of the question

--- learn-LearnOpenGL/Source.cpp
+++ learn-LearnOpenGL/Source.cpp
@@ -1,3 +1,10 @@
+/********************************************************************************************************/
+/* EXERCISES-3:                                                                                         */
+/* --Output the vertex position to the fragment shader using the out keyword and set the fragment's     */
+/*   color equal to this vertex position (see how even the vertex position values are interpolated      */
+/*   across the triangle). Once you managed to do this; try to answer the following question: why is    */
+/*   the bottom-left side of our triangle black?                                                        */
+/********************************************************************************************************/
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
