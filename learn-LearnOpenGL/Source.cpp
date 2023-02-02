ceac7ad0b02be5c7ea5e08e7fccb41872fa3cb45
Doner357 <don468sam@gmail.com>
2023-02-02 18:42:29 +0800

    Change paths name

--- learn-LearnOpenGL/Source.cpp
+++ learn-LearnOpenGL/Source.cpp
@@ -90,7 +90,7 @@ int main(void) {
 	* --------------------------------------------------------------------------------------------------------------------
 	*/
 
-	Shader ourShader("shaders/vertex/vShader.vs", "shaders/fragment/fShader.fs");
+	Shader ourShader("shaders/vertex/vShader.vert", "shaders/fragment/fShader.frag");
 
 
 
